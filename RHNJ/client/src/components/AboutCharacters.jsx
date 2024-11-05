7dd9209cc10c3e45f58ceb6f06274175de647ada
Katherine Joey <katherinejoey@katherines-air.mynetworksettings.com>
2024-11-04 20:40:19 -0500

    edited Admin

--- RHNJ/client/src/components/AboutCharacters.jsx
+++ RHNJ/client/src/components/AboutCharacters.jsx
@@ -6,7 +6,7 @@ import Navigations from './Navigations';
 // import AboutCharacters from './AboutCharacters';
 
 export default function AboutCharacters({
-  setSelectedCharacterID,
+  // setSelectedCharacterID,
   characters,
 }) {
   return (
@@ -16,8 +16,12 @@ export default function AboutCharacters({
         characters.map((character) => (
           <Link to={`/character/${character.id}`} key={character.id}>
             <div>
-              <img src={character.image} alt={character.class} className='char-img' />
-              <h3>{character.class}</h3>
+              <img
+                src={character.image}
+                alt={character.characterClass}
+                className='char-img'
+              />
+              <h3>{character.characterClass}</h3>
             </div>
           </Link>
         ))
