4e93ef6411bfe0043dfe36875a4f920afae8b1b7
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-11-07 20:25:55 -0500

    user character displays

--- RHNJ/client/src/components/UserCharacter.jsx
+++ RHNJ/client/src/components/UserCharacter.jsx
@@ -22,13 +22,65 @@ const UserCharacter = () => {
     },[])
   return (
       <>
-      <h1>character placeholder</h1>
+      
     {
         characterInfo &&(
-            <div>
+            <div className='character-stats'>
 
-           <h3>{characterInfo.character.characterName}</h3>
-           <h3>{characterInfo.character.flaws}</h3>
+           <h1>{characterInfo.character?.characterName}</h1>
+           <p> Catch Phrases:{' '}
+            {characterInfo.character.catchPhrases.map((phrase, index) => (
+              <span key={index}>
+                {phrase}
+                {index < characterInfo.character.catchPhrases.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+          </p>
+
+           <h3>Level:{characterInfo.character?.level} </h3>
+           <p>Status Points: {characterInfo.character?.statusPoints}</p>
+           <h3>Stats:</h3>
+           <ul>
+            <li>Strength: {characterInfo.character.attributes.strength}</li>
+            <li>Dexterity: {characterInfo.character.attributes.dexterity}</li>
+            <li>Constitution: {characterInfo.character.attributes.constitution}</li>
+            <li>Intelligence: {characterInfo.character.attributes.intelligence}</li>
+            <li>Wisdom: {characterInfo.character.attributes.wisdom}</li>
+            <li>Charisma: {characterInfo.character.attributes.charisma}</li>
+           </ul>
+         <p> Saving Throws:{' '}
+            {characterInfo.character.attributes?.savingThrows?.map((save, index) => (
+              <span key={index}>
+                {save}
+                {index < characterInfo.character.attributes.savingThrows.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+          </p>
+          
+          <p className='char-txt'>
+            Skills:{' '}
+            {characterInfo.character.skills?.map((skill, index) => (
+              <span key={index}>
+                {skill}
+                {index < characterInfo.character.skills.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+            </p>
+            <p className='char-txt'>
+            Single-Use Skills:{' '}
+            {characterInfo.character.singleUseSkill?.map((skill, index) => (
+              <span key={index}>
+                {skill}
+                {index < characterInfo.character.singleUseSkill.length - 1
+                  ? ', '
+                  : ''}
+              </span>
+            ))}
+          </p> 
+          <p>Attack Roll: {characterInfo.character.attackRoll}</p>
+           <h3>Flaws: {characterInfo.character.flaws}</h3>
+           <h3>Ideals: {characterInfo.character.ideals}</h3>
+           <h3>Notes: {characterInfo.character.notes}</h3>
             </div>
         )
         }
